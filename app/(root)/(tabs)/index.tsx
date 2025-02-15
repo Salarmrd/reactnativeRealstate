27ff078d77a78f923ae88f8e5a85f8f6a8bb74d1
Amirsalar <Amirsalarmehrdad22@gmail.com>
2025-02-16 00:09:58 +0300

    sign in page added and added images and icons

---
+++ app/(root)/(tabs)/index.tsx
@@ -0,0 +1,22 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text className="font-bold font-rubik text-3xl my-10">
+        Welcome to ReState
+      </Text>
+      <Link href="/sign-in">Sign in</Link>
+      <Link href="/explore">Explore</Link>
+      <Link href="/profile">Profile</Link>
+      <Link href="/property/1">property</Link>
+    </View>
+  );
+}
